1ed8fc211371a1bb15f1f46a5efec9be33ac19d0
Michael Kwaku Aboagye <35949773+Rev0kz@users.noreply.github.com>
2019-03-17 08:16:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,132 @@
-import connexion 
-import json 
-from flask import jsonify 
-import sqlite3 
-
-#route handler for apiversion
-@app.route("/api/version/info", method=["GET"])
-def api_info():
-  conn = sqlite3.connect('lib.db')
-  print("opened database successfully");
-  api_info = []
-  cursor = conn.execute("SELECT buildtime, version, links, methods from apiversion")
-  
-for row in cursor:
-  a_dict = {}
-  a_dict = ['version'] = row[0]
-  a_dict = ['buildtime'] = row[1]
-  a_dict = ['links'] = row[2]
-  a_dict = ['methods'] = row[3]
-  api_list.append(a_dict)
-  
-conn.close()
-return jsonify({'api_info' : api_list})      
+from flask import Flask
 
 
-#route handler to retrieve a list of authors in the authors table" 
 
-@app.route("api/v1/authors", method=["GET"])
-def list_author():
-    return get_author
-  
-#logic to retrieve list of authors from the authors table
-
-def get_author():
-  conn = sqlite3.connect("library.db")
-  authors = []
-  cursor = conn.execute("SELECT name, country, email, books, id from authors")
+@app.route('/api/v2/info', methods=["GET"])
+def get_info():
+  conn = sqlite3.connect('library.db')
+  print("connected to library the database")
+  api_release = []
+  cursor = conn.execute("SELECT buildtime, version, methods from apiinfo")
   for row in cursor:
-  author_data = {}
-  a_dict['name'] = row[0]
-  a_dict['country'] = row[1]
-  a_dict['email'] = row[2]
-  a_dict['book'] = row[3]
-  a_dict['id'] = row[4]
-  authors.append(author_data)
+    api_info = {}
+    api_info['buildtime'] = row[0]
+    api_info['version'] = row[1]
+    api_info['methods'] = row[2]
   conn.close()
-      return jsonify({'list_of_authors': authors))
+  return jsonify({'api_release': api_info}), 200
+
+
+
+
+@app.route('/api/v2/authors' methods=['GET'])
+def list_authors():
+  return show_authors() 
+
+
+def show_authors()
+conn = sqlite3.connect('library.db')
+print ("database connected successfully")
+authors_list=[]
+cursor = conn.execute("SELECT name, country, email, books, from Author")
+for row in cursor:
+data_dict = {}
+data_dict["name"] = row[0]
+data_dict["country"] = row[1]
+data_dict["email"] = row[2]
+data_dict["book"] = row[3]
+authors_list.append(data_dict)
+conn.close()
+  return jsonify({'show_authors': authors_list})
+
+
+
+@app.route('/api/v2/author<author_name>', method=['GET'])
+def get_author(author_name):
+  return author_info(author_name)
+
+
+def author_info(author_name):
+conn = sqlite3.connect("library.db")
+author_data = []
+cursor = conn.execute(SELECT * from Author where name=?", (author_name,))
+data = cursor.fetchall()
+if len(data) == 0:
+    abort(404) 
+else: 
+  author = {}
+  author['name'] = data[0][0]
+  author['country'] = data[0][1]
+  author['email'] = data[0][2]
+  author['book'] = data[0][3]
+      conn.close()
+      return jsonify({a_dict})
                       
                       
- #route handler to retrieve a specific author from the authors table                     
- @app.route("api/v2/authors                     
+
+
+      author['email'] = data[0][2]
+      author['book'] = data[0][3]
+          conn.close()
+          return jsonify({a_dict})
+
+
+@app.errorhandler(404)
+def author_not_found(error):
+      return make_response(jsonify({'error': 'author not available!'}), 404) 
+                      
+ 
+@app.route('/api/v2/author', methods=['POST'])
+def newauthors():
+   if not request.json or 'name' not in request.json:
+      abort(400)
+   user = { 
+        'name' = request.json['name'],
+        'country' = request. json['country'],
+        'email' = request.json['email'],
+        'book' = request.json['book']
+        }
+    return jsonify({    })
+                      
+                      
+
+def include_author():
+     conn = connect.sqlite3("library.db")
+     print("database connected")
+     author_data = []
+     cursor = conn.cursor()
+     cursor.execute(SELECT * from Author where name=?,  (new_author['name'])) 
+     data = cursor.fetchall()
+        if len(data) != 0: 
+                abort(400)
+        else:
+          cursor.execute("insert into library (author, country, book) values(?,?,?)", (new_author['name'], new_author['country'], new_author['email'], new_author['books']))
+          conn.commit()
+          return "created"
+       conn.close()
+       return jsonify(a_dict)
+                      
+                      
+ 
+@app.route('/api/v2/authors', methods=['DELETE'])
+def del():  
+    if not request.json or 'name' not in request.json: 
+          abort(400)
+    author = request.json['name']
+    return jsonify({'status': delete_user(author)}), 200 
+
+def delete_user(delete_user): 
+    conn = connect.sqlite3("library.db")
+    print("database connected successfully");
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from Author where name=? ", (delete_user,)) 
+    details = cursor.fetchall()
+    print("Details", details)
+    if len(details) == 0:  
+        abort(400);
+    else:
+      cursor.execute("delete from Author where name=?", (delete_user,))
+      conn.commit()
+      return "author successfully deleted"                 
+                      
+                      
